3709edee40ab7cfb3978f76e86649d15892360d3
Daniel Goetz <daniel.goetz@rio.cloud>
2017-11-17 13:50:32 +0100

    removed User group as for the beginning usage of the Google User group is sufficient

--- src/main/java/de/ase11/attendanceTrackingSystem/App.java
+++ src/main/java/de/ase11/attendanceTrackingSystem/App.java
@@ -8,9 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        User testUser = new User("test@example.de", "Test User");
-        System.out.println(testUser.getEmail());
-        System.out.println(testUser.getUser_id());
+        //System.out.println( "Hello World!" );
+
     }
 }
--- src/main/java/de/ase11/attendanceTrackingSystem/OfyHelper.java
+++ src/main/java/de/ase11/attendanceTrackingSystem/OfyHelper.java
@@ -32,7 +32,6 @@ public class OfyHelper implements ServletContextListener {
     // This will be invoked as part of a warmup request, or the first user request if no warmup
     // request.
     ObjectifyService.register(Group.class);
-    //ObjectifyService.register(User.class);
   }
 
   public void contextDestroyed(ServletContextEvent event) {
