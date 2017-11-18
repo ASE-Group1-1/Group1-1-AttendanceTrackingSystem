b95ffa900752b4df45ddf4b0bde1727049da144d
Daniel Goetz <daniel.goetz@rio.cloud>
2017-11-19 00:14:38 +0100

    added XML schema info to the package info file

---
+++ src/main/java/de/ase11/attendanceTrackingSystem/package-info.java
@@ -0,0 +1,10 @@
+@XmlSchema(
+        xmlns = {},
+        namespace = "http://www.w3.org/2001/XMLSchema",
+        elementFormDefault = XmlNsForm.UNSET,
+        attributeFormDefault = XmlNsForm.UNSET
+)
+package de.ase11.attendanceTrackingSystem;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
