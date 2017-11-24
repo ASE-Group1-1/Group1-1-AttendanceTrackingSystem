580c597af33f119d416a71e6aaa0e2950c49a154
Daniel Goetz <daniel.goetz@rio.cloud>
2017-11-24 16:42:17 +0100

    Refactoring. Renamed AttendanceLogApplication to AttendanceApplication

--- src/main/java/de/ase11/attendanceTrackingSystem/AttendenceApplication.java
+++ src/main/java/de/ase11/attendanceTrackingSystem/AttendenceApplication.java
@@ -11,7 +11,7 @@ import org.restlet.data.MediaType;
 import org.restlet.routing.Router;
 
 
-public class AttendenceLogApplication extends Application {
+public class AttendenceApplication extends Application {
 
     /**
      * Creates a root Restlet that will receive all incoming calls.
