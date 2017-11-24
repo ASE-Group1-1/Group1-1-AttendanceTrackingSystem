8f01a2671cd86e24c297c56cb98cbe30df3f10be
Daniel Goetz <daniel.goetz@rio.cloud>
2017-11-24 22:10:15 +0100

    Refactoring. Fixed typo in the class name AttendanceApplication

--- src/main/java/de/ase11/attendanceTrackingSystem/rest/AttendanceApplication.java
+++ src/main/java/de/ase11/attendanceTrackingSystem/rest/AttendanceApplication.java
@@ -16,8 +16,7 @@ import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 
-// TODO: Remove typo from classname
-public class AttendenceApplication extends Application {
+public class AttendanceApplication extends Application {
 
     /**
      * Creates a root Restlet that will receive all incoming calls.
